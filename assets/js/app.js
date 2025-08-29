a92ab06a9c30ca66538f674a43e885755badae72
gajanankdm <gajanankadam0965@gmail.com>
2025-08-29 22:33:23 +0530

    change on js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,13 +16,14 @@ const  digitclock = () =>{
 let d = new Date();
 
 let hr = d.getHours();
-
+let session = "PM"
 if(hr>12){
     hr=hr-12
+    session="AM"
 }
 let min = d.getHours();
 let sec = d.getSeconds();
-let session = "PM"
+
 
 let result = `${setZero(hr)} : ${setZero(min)} : ${setZero(sec)} ${session}`;
 
@@ -35,3 +36,5 @@ let result = `${setZero(hr)} : ${setZero(min)} : ${setZero(sec)} ${session}`;
 
 
 digitclock()
+
+
