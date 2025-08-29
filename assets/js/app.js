b3fcf7c8b71c7ea95e5ee874c6db7dadb9430142
gajanankdm <gajanankadam0965@gmail.com>
2025-08-29 20:58:22 +0530

    work on ui branch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,37 @@
 const cl = console.log;
 
+const setZero = (num) =>{
+    if(num<10){
+        return "0" + num
+    }else{
+        return num
+    }
+}
 
 
+
+const  digitclock = () =>{
+    
+
+let d = new Date();
+
+let hr = d.getHours();
+
+if(hr>12){
+    hr=hr-12
+}
+let min = d.getHours();
+let sec = d.getSeconds();
+let session = "PM"
+
+let result = `${setZero(hr)} : ${setZero(min)} : ${setZero(sec)} ${session}`;
+
+ document.getElementById('digitalClock').innerHTML=result
+
+  setTimeout(() => {
+    digitclock()
+  }, 1000);
+}
+
+
+digitclock()
